c78d32b284a4dcc9031912d6332942ddd04f7c96
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-09-08 18:40:23 +0300

    Create todoList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { Heading, VStack } from "@chakra-ui/react";
 import "./App.css";
 import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <VStack p={4} minH="100vh">
-      <Heading mt={20} p={5} fontWeight="extrabold" size="xl" color="blue.400">
       <Heading mt={15} p={5} fontWeight="extrabold" size="2xl" color="blue.400">
         Todo List
       </Heading>
       <AddTodo />
+      <TodoList />
     </VStack>
   );
 }
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,50 @@
+import {
+  Flex,
+  HStack,
+  Image,
+  StackSeparator,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import ClearTodoList from "./ClearTodoList";
+import DeleteTodo from "./DeleteTodo";
+import img from "../assets/EmptyTodoList.jpg";
+
+export default function TodoList() {
+  return (
+    <>
+      <Flex justify="center">
+        <Image src={img} maxW="35%" />
+      </Flex>
+      <VStack
+        separator={<StackSeparator />}
+        borderColor="blue.100"
+        borderWidth="2px"
+        p="5"
+        borderRadius="lg"
+        w="100%"
+        maxW={{ base: "90vw", sm: "80vw", lg: "50vw", xl: "30vw" }}
+        alignItems="stretch"
+      >
+        <HStack>
+          <Text w="100%" p="8px" borderRadius="lg" color="blue.400">
+            Work on task no.2
+          </Text>
+          <DeleteTodo />
+        </HStack>
+        <HStack>
+          <Text w="100%" p="8px" borderRadius="lg" color="blue.400">
+            Buy food for lunch
+          </Text>
+          <DeleteTodo />
+        </HStack>
+        <HStack>
+          <Text w="100%" p="8px" borderRadius="lg" color="blue.400">
+            Visit my friend
+          </Text>
+          <DeleteTodo />
+        </HStack>
+      </VStack>
+    </>
+  );
+}

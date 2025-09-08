4cef3d71557997faa0399d3d98afc22ff709aacd
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-09-08 17:09:23 +0300

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Heading, VStack } from "@chakra-ui/react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <VStack p={4} minH="100vh">
+      <Heading mt={20} p={5} fontWeight="extrabold" size="xl" color="blue.400">
+        Todo List
+      </Heading>
+    </VStack>
+  );
 }
 
-export default App
+export default App;

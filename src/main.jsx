460f38cfbfcceecb30bfc7001d8f23e8d4791086
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-09-10 23:49:13 +0300

    Update main.jsx
    added todo provider to app

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Toaster } from "./components/ui/toaster";
+import { TodoProvider } from "./contexts/TodoContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ChakraProvider value={defaultSystem}>
-      <App />
-      <Toaster />
+      <TodoProvider>
+        <App />
+        <Toaster />
+      </TodoProvider>
     </ChakraProvider>
   </StrictMode>
 );

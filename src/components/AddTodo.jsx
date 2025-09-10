3386460d3fb311fd3e34b7e9d7c7389c45dc1726
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-09-10 23:50:18 +0300

    Update AddTodo.jsx
    used todo context in add todo component

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,27 +1,38 @@
+import { useTodo } from "@/contexts/TodoContext";
 import { Button, HStack, Input } from "@chakra-ui/react";
+import { useState } from "react";
 
 export default function AddTodo() {
+  const [input, setInput] = useState("");
+  const { addTodo } = useTodo();
+
+  const handleOnClick = () => {
+    console.log("AddTodo: Adding todo");
+    addTodo(input);
+    setInput("");
+  };
+
   return (
-    <form>
-      <HStack h={45}>
-        <Input
-          h="100%"
-          variant="outline"
-          placeholder="What are you up for today?"
-          colorPalette="blue"
-          w="xs"
-        />
-        <Button
-          size="sm"
-          color="white"
-          bgColor="blue.500"
-          px={10}
-          h="100%"
-          type="submit"
-        >
-          Add
-        </Button>
-      </HStack>
-    </form>
+    <HStack h={45}>
+      <Input
+        h="100%"
+        variant="outline"
+        placeholder="What are you up for today?"
+        colorPalette="blue"
+        w="xs"
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <Button
+        size="sm"
+        color="white"
+        bgColor="blue.500"
+        px={10}
+        h="100%"
+        type="submit"
+        onClick={handleOnClick}
+      >
+        Add
+      </Button>
+    </HStack>
   );
 }

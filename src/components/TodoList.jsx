272b94bbdb313e572df621249b157ae8d556b74a
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-09-11 06:34:10 +0300

    Update TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,10 @@
 import {
   Flex,
+  Heading,
   HStack,
   Image,
+  Skeleton,
+  Spinner,
   StackSeparator,
   Text,
   VStack,
@@ -10,9 +13,42 @@ import ClearTodoList from "./ClearTodoList";
 import DeleteTodo from "./DeleteTodo";
 import img from "../assets/todo.jpg";
 import { useTodo } from "@/contexts/TodoContext";
+import { useEffect, useState } from "react";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export default function TodoList() {
   const { todoList } = useTodo();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (todoList) {
+      setTimeout(() => {
+        setLoading(false);
+      }, 3000);
+    }
+  }, [todoList]);
+
+  if (loading) {
+    return (
+      <>
+        <DotLottieReact
+          src="https://lottie.host/1d57a315-e7df-4284-b98f-11271d96febf/Mlv6dHYkpZ.lottie"
+          loop
+          autoplay
+          style={{ width: "40%", height: "auto" }}
+        />
+        <Heading
+          mt={15}
+          p={5}
+          fontWeight="extrabold"
+          size="xl"
+          color="blue.400"
+        >
+          Fetching todo list <Spinner />
+        </Heading>
+      </>
+    );
+  }
 
   return (
     <>

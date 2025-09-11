996fc27d46930894e81e90764d6434da2e197d82
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-09-11 06:18:25 +0300

    Update DeleteTodo.jsx

--- src/components/DeleteTodo.jsx
+++ src/components/DeleteTodo.jsx
@@ -1,7 +1,6 @@
 import { IconButton, Spinner } from "@chakra-ui/react";
 import { useState } from "react";
 import { FiTrash2 } from "react-icons/fi";
-import { toaster } from "./ui/toaster";
 import { useTodo } from "@/contexts/TodoContext";
 
 export default function DeleteTodo({ id }) {
@@ -9,19 +8,11 @@ export default function DeleteTodo({ id }) {
   const { deleteTodo } = useTodo();
 
   const handleDelete = async () => {
-    console.log("DeleteTodo: Deleting todo");
     setLoading(true);
-    console.log("loading is now TRUE");
-
     setTimeout(() => {
       deleteTodo(id);
-      toaster.create({
-        title: "Todo deleted",
-        type: "success",
-      });
       setLoading(false);
-      console.log("loading is now FALSE");
-    }, 3000);
+    }, 1000);
   };
 
   return (

29e7aca22c3edd39cf2c0d5bc9ca9913c219050f
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-09-14 08:34:05 +0300

    Create ScrollToTopButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import { Heading, VStack } from "@chakra-ui/react";
+import { Box, Heading, VStack } from "@chakra-ui/react";
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
 import { useTodo } from "./contexts/TodoContext";
 import ErrorAlert from "./components/ui/ErrorAlert";
+import ScrollToTopButton from "./components/ScrollToTopButton";
 
 function App() {
   const { error, setError } = useTodo();
 
   return (
-    <VStack p={4} minH="100vh">
-      <Heading mt={15} p={5} fontWeight="extrabold" size="3xl" color="blue.400">
-        Todo App
-      </Heading>
-      {error && <ErrorAlert error={error} setError={setError} />}
-      <AddTodo />
-      <TodoList />
-    </VStack>
+    <>
+      <VStack p={4} minH="100vh">
+        <Heading p={5} fontWeight="extrabold" size="3xl" color="blue.400">
+          Todo App
+        </Heading>
+        {error && <ErrorAlert error={error} setError={setError} />}
+        <AddTodo />
+        <TodoList />
+      </VStack>
+      <ScrollToTopButton />
+    </>
   );
 }
 
---
+++ src/components/ScrollToTopButton.jsx
@@ -0,0 +1,37 @@
+import { IconButton } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { HiArrowNarrowUp } from "react-icons/hi";
+
+export default function ScrollToTopButton() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setShow(window.scrollY > 200);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  if (!show) return null;
+
+  return (
+    <IconButton
+      onClick={handleClick}
+      position="fixed"
+      bottom="20px"
+      right="20px"
+      bgColor="blue.400"
+      aria-label="Scroll to top"
+      borderRadius="full"
+      boxShadow="md"
+      zIndex="50"
+    >
+      <HiArrowNarrowUp />
+    </IconButton>
+  );
+}

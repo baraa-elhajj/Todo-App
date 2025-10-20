3397666c52ab59fd930bac251ff3dcca4a96ea5c
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-10-20 22:48:09 +0300

    Update TodoList.jsx
    Moved loading screen to separate component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,54 +1,11 @@
-import {
-  Box,
-  Flex,
-  Heading,
-  Image,
-  Spinner,
-  StackSeparator,
-  VStack,
-} from "@chakra-ui/react";
+import { Flex, Heading, Image, StackSeparator, VStack } from "@chakra-ui/react";
 import ClearTodoList from "./ClearTodoList";
 import img from "../assets/todo.jpg";
 import { useTodo } from "@/contexts/TodoContext";
-import { useEffect, useState } from "react";
-import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import TodoItem from "./TodoItem";
 
 export default function TodoList() {
   const { todoList } = useTodo();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    if (todoList) {
-      setTimeout(() => {
-        setLoading(false);
-      }, 3000);
-    }
-  }, [todoList]);
-
-  if (loading) {
-    return (
-      <>
-        <Box w={{ base: "100%", md: "40%" }} h="auto" mx="auto">
-          <DotLottieReact
-            src="https://lottie.host/1d57a315-e7df-4284-b98f-11271d96febf/Mlv6dHYkpZ.lottie"
-            loop
-            autoplay
-            style={{ width: "100%", height: "100%" }}
-          />
-        </Box>
-        <Heading
-          mt={15}
-          p={5}
-          fontWeight="extrabold"
-          size="xl"
-          color="blue.400"
-        >
-          Fetching todo list <Spinner />
-        </Heading>
-      </>
-    );
-  }
 
   return (
     <>

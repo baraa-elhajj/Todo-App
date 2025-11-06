b7631cab3e508746edfc5edde9c94c8ca3a03d2d
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-11-06 23:04:53 +0200

    Use EditableItem to update todos

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,8 +1,8 @@
 import { HStack, IconButton, Text } from "@chakra-ui/react";
 import DeleteTodo from "./DeleteTodo";
-import EditTodo from "./EditTodo";
 import { useTodo } from "@/contexts/TodoContext";
-import { FaCheck } from "react-icons/fa6";
+import { FaRegCircleCheck } from "react-icons/fa6";
+import EditableItem from "./ui/EditableItem";
 
 export default function TodoItem({ todo }) {
   const { completeTodo } = useTodo();
@@ -20,19 +20,16 @@ export default function TodoItem({ todo }) {
       borderColor="blue.200"
       bgColor={todo.completed ? "#eef8feff" : "white"}
     >
-      <Text w="100%" p="8px" color="blue.400">
-        {todo.text}
-      </Text>
-
-      <DeleteTodo id={todo.id} />
-      <EditTodo todo={todo} />
+      <EditableItem todo={todo} />
       <IconButton
         rounded="full"
-        bgColor={todo.completed ? "green.400" : "blue.400"}
+        bgColor="transparent"
+        color={todo.completed ? "green.400" : "blue.400"}
         onClick={handleOnClick}
       >
-        <FaCheck />
+        <FaRegCircleCheck />
       </IconButton>
+      <DeleteTodo id={todo.id} />
     </HStack>
   );
 }
